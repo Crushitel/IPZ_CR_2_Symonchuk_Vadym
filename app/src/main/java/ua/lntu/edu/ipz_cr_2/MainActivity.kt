1488f445a2cff7a9b2ccbc0b58f0df1f4e6c178b
Vadym <Sum.vadim415579suq@gmail.com>
2024-03-12 14:15:57 +0200

    Create variables for email, password and status signed

--- app/src/main/java/ua/lntu/edu/ipz_cr_2/MainActivity.kt
+++ app/src/main/java/ua/lntu/edu/ipz_cr_2/MainActivity.kt
@@ -8,9 +8,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import ua.lntu.edu.ipz_cr_2.ui.theme.IPZ_CR_2Theme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +33,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SignSystem()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SignSystem() {
+    SignInScreen()
+}
+
+@Composable
+fun SignInScreen(){
+    var email by remember { mutableStateOf(TextFieldValue()) }
+    var password by remember { mutableStateOf(TextFieldValue()) }
+    var signedIn by remember { mutableStateOf(false) }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SignInpreview() {
     IPZ_CR_2Theme {
-        Greeting("Android")
+        SignSystem()
     }
 }
